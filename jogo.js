44836d54cf4bac4948cfbba6e1adc37ce16b5f04
lucaasemanueel <lucas.emanuel@dce.ufpb.br>
2020-08-14 20:01:10 -0300

    Remoção do icone a cada ciclo de tempo

--- jogo.js
+++ jogo.js
@@ -4,7 +4,7 @@ var largura = 0
 function ajustaTamanhoPalcoJogo(){
 	altura = window.innerHeight
 	largura = window.innerWidth
-	console.log(altura, largura)
+	console.log(largura, altura)
 }
 
 
@@ -12,6 +12,13 @@ ajustaTamanhoPalcoJogo()
 
 
 function posicaoRandomica(){
+
+	//remover mosquito anterior (caso exista)
+	if(document.getElementById('mosquito')){
+		document.getElementById('mosquito').remove()
+	}
+	
+
 	var posicaoX = Math.floor(Math.random() * largura) - 90
 	var posicaoY = Math.floor(Math.random() * altura) - 90
 	
@@ -25,19 +32,18 @@ function posicaoRandomica(){
 
 	var mosquito = document.createElement('img')
 	mosquito.src = 'imagens/mosca.png'
-	mosquito.className = tamanhoAleatorio()
+	mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
 	mosquito.style.left = posicaoX + 'px'
 	mosquito.style.top = posicaoY + 'px'
 	mosquito.style.position = 'absolute'
-
+	mosquito.id = 'mosquito'
 	document.body.appendChild(mosquito)
-	
  
 }
 
 function tamanhoAleatorio(){
 	var classe = Math.floor(Math.random() * 3)
-	console.log(classe)
+	
 
 	switch(classe){
 		case 0:
@@ -46,5 +52,17 @@ function tamanhoAleatorio(){
 			return 'mosquito2'
 		case 2:
 			return 'moquito3'
+	}
+}
+
+function ladoAleatorio(){
+
+	var classe = Math.floor(Math.random() * 2)
+
+	switch(classe){
+		case 0:
+			return 'ladoA'
+		case 1:
+			return 'ladoB'
 	}
 }
