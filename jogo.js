9da34427ba11fc6031c44af0efa6175797ed740f
lucaasemanueel <lucas.emanuel@dce.ufpb.br>
2020-08-17 23:29:35 -0300

    Construindo a pag do gameOver e demais funcionalidades

--- jogo.js
+++ jogo.js
@@ -1,5 +1,6 @@
 var altura = 0
 var largura = 0
+var vidas = 1
 
 function ajustaTamanhoPalcoJogo(){
 	altura = window.innerHeight
@@ -16,6 +17,17 @@ function posicaoRandomica(){
 	//remover mosquito anterior (caso exista)
 	if(document.getElementById('mosquito')){
 		document.getElementById('mosquito').remove()
+
+		//console.log('Elemento selecionado foi v' + vidas)
+		if(vidas > 3){
+			window.location.href = 'fim_de_jogo.html'
+		}else{
+			document.getElementById('v'+ vidas).src='imagens/coracao_vazio.png'
+			vidas++
+		}
+			
+		 
+		
 	}
 	
 
@@ -37,6 +49,10 @@ function posicaoRandomica(){
 	mosquito.style.top = posicaoY + 'px'
 	mosquito.style.position = 'absolute'
 	mosquito.id = 'mosquito'
+	mosquito.onclick = function() {
+		this.remove()
+	}
+
 	document.body.appendChild(mosquito)
  
 }
